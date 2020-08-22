6b08dd7d376b4c484e4d79367028282deb71ac77
Zahedul Alam <z.alam015@gmail.com>
2020-08-22 22:52:44 +0600

    initial

---
+++ android/app/src/main/kotlin/com/example/samplechats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samplechats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/chat.screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This is chat'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance.collection('chats/VEkiETleAv7BfjlsPiMY/messages');
+        },
+      ),
+    );
+  }
+}
